89d76dd6923ee594f56c0f560ae03300f2b18d37
Ilya Volodkevich <aimmaggot@gmail.com>
2020-03-31 00:54:43 +0300

    completed 3 task

--- 1.js
+++ 1.js
@@ -1,8 +1,32 @@
-let money = 1;
+let money = +prompt('Ваш месячный доход?','');
 let income = 2;
-let addExpenses = 3;
-let deposit = 4;
-let mission = 5;
+let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую','');
+let deposit = confirm('Есть ли у вас депозит в банке?');
+let mission = 50000;
 let period = 5;
-alert('И И У А А ');
-console.log('hello');
+let expenses1 = prompt('Введите обязательную статью расходов?','');
+let amount1 = +prompt('Во сколько это обойдется?','');
+let expenses2 = prompt('Введите обязательную статью расходов?','');
+let amount2 = +prompt('Во сколько это обойдется?','');
+
+let budgetMonth = money - (amount1 + amount2); 
+let budgetDay = budgetMonth / 30;
+console.log (budgetMonth);
+console.log(Math.round(mission / budgetMonth));
+console.log(Math.floor(budgetDay));
+
+if (budgetDay > 1200) {
+    alert('У вас высокий уровень дохода');
+
+} else if (budgetDay > 600 || budgetDay < 1200) {
+    
+    alert('У вас средний уровень дохода');
+
+} else if (budgetDay < 600) {
+    
+    alert('К сожалению у вас уровень дохода ниже среднего')
+} else if (budgetDay = -budgetDay) {
+    alert('Что-то пошла не так');
+};
+
+
