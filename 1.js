55b78b1c964b361be2e788ffdaab17dfd8beebee
Ilya Volodkevich <aimmaggot@gmail.com>
2020-04-01 17:38:14 +0300

    lesson 4 commit

--- 1.js
+++ 1.js
@@ -1,19 +1,45 @@
+'use strict';
 let money = +prompt('Ваш месячный доход?','');
 let income = 2;
 let addExpenses = prompt('Перечислите возможные расходы за рассчитываемый период через запятую','');
 let deposit = confirm('Есть ли у вас депозит в банке?');
 let mission = 50000;
 let period = 5;
-let expenses1 = prompt('Введите обязательную статью расходов?','');
+let expenses1 = +prompt('Введите обязательную статью расходов?','');
 let amount1 = +prompt('Во сколько это обойдется?','');
-let expenses2 = prompt('Введите обязательную статью расходов?','');
+let expenses2 = +prompt('Введите обязательную статью расходов?','');
 let amount2 = +prompt('Во сколько это обойдется?','');
 
-let budgetMonth = money - (amount1 + amount2); 
-let budgetDay = budgetMonth / 30;
-console.log (budgetMonth);
-console.log(Math.round(mission / budgetMonth));
-console.log(Math.floor(budgetDay));
+
+function getExpensesMonth (a, b,) {
+    return a + b;
+
+}
+
+let resExpensesMonth = getExpensesMonth(amount1, amount2);
+
+console.log(resExpensesMonth);
+
+function getAccumulatedMonth(a, b, ) {
+return a - b;
+}
+
+let AccumulatedMonth = getAccumulatedMonth (money, resExpensesMonth);
+
+console.log (AccumulatedMonth);
+
+function getTargetMonth (a, b) {
+    return a / b;
+}
+let TargetMonth = getTargetMonth (mission, AccumulatedMonth);
+
+console.log(TargetMonth);
+
+
+
+let budgetDay = AccumulatedMonth / 30;
+
+
 
 if (budgetDay > 1200) {
     alert('У вас высокий уровень дохода');
@@ -28,5 +54,11 @@ if (budgetDay > 1200) {
 } else if (budgetDay = -budgetDay) {
     alert('Что-то пошла не так');
 };
+
+
+
+
+
+
 
 
